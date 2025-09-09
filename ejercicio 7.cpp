9d7bdc0b0767b384f0bbb814066851db3d2ad956
ingNeva <alexander.neva@utp.edu.co>
2025-09-09 16:31:23 -0500

    Update ejercicio 7.cpp

--- ejercicio 7.cpp
+++ ejercicio 7.cpp
@@ -1,18 +1,1 @@
-/*
 
-5. Leer 10 números enteros, almacenarlos en un vector. Luego leer un entero y determinar si 
-este último entero se encuentra entre los 10 valores almacenados en el vector.
-
-
-*/
-
-
-
-#include <iostream>
-#include <cstdio>
-
-int main(){
-
-0
-
-}
